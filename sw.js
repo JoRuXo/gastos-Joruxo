737b39721857fb19f4722e0b30e2f260947ad647
JoRuXo <JoRuXo@hotmail.com>
2026-06-21 15:39:26 +0200

    Update cache version from v6 to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'mis-gastos-v6';
+var CACHE = 'mis-gastos-v7';
 var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
